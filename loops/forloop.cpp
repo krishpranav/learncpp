5632996160307bf4787653e6aea3d2030a60e51f
krishpranav <krisna.pranav@gmail.com>
2021-09-24 18:33:49 +0530

    loops: forloop
    learn how to loop a process using for loop

---
+++ loops/forloop.cpp
@@ -0,0 +1,20 @@
+/**
+ * learn to loop a process using for loop
+ */
+
+#include <iostream>
+
+int main()
+{
+    /**
+     * what this for loop will do??
+     * this will store integer x as 1
+     * it will increment it by 1 till it reaches 20 :)
+     */
+
+    for (int a =  1; a < 20; a = a + 1) {
+        std::cout << "value of a: " << a << std::endl;
+    }
+
+    return 0;
+}
