8fee756b43a46a782d2d13ad0dc211096673d04c
krishpranav <krisna.pranav@gmail.com>
2021-09-24 17:18:13 +0530

    helloworld: first example

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,14 @@
+// includes
+#include <iostream>
+
+// call the std namesapce 
+using namespace std;
+
+int main() 
+{
+    // print the text 
+    cout << "hello world";
+
+    // returns 0
+    return 0;
+}
