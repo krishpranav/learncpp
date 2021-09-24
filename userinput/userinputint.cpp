f546cafb42cd9b62abb8b1b1ff3845874777e776
krishpranav <krisna.pranav@gmail.com>
2021-09-24 19:09:15 +0530

    userinput: userinput int
    get integer from user and print it out

---
+++ userinput/userinputint.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+
+    std::cout << "Enter a number: ";
+    std::cin >> x;
+
+    std::cout << "The number you have entered is " << x;
+
+    return 0;
+}
