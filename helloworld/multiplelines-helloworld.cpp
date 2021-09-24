81f7850b406720fdd9b8d64f452e10476ee6f199
krishpranav <krisna.pranav@gmail.com>
2021-09-24 17:26:22 +0530

    helloworld in multiple lines
    learn to print a text more than 1 times :)

---
+++ helloworld/multiplelines-helloworld.cpp
@@ -0,0 +1,16 @@
+/**
+ * learn to print a text more than 1 times
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello world\n";
+    std::cout << "hello world\n";
+    std::cout << "hello world\n";
+    std::cout << "hello world\n";
+    std::cout << "hello world\n";
+
+    return 0;
+}
