b39a2a32978925ba77668a442ea1fcde3f42008f
krishpranav <krisna.pranav@gmail.com>
2021-09-24 18:10:25 +0530

    variables: int
    learn how to store numbers

---
+++ variables/int.cpp
@@ -0,0 +1,22 @@
+/**
+ * learn how to store numbers
+ */
+
+#include <iostream>
+
+int main()
+{
+    /**
+     * store a integer using int;
+     * int yourinteger i have mentioned my integer as x
+     * int x = 100;
+     */
+
+    int x = 100;
+
+    /**
+     * to check that your variable has been stored in x successfully print it
+     * using std::cout << x;
+     */
+    std::cout << x;
+}
