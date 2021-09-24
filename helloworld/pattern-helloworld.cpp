b1ce3371448c40a4ac5e50ac61aa6435b014ed56
krishpranav <krisna.pranav@gmail.com>
2021-09-24 17:38:50 +0530

    helloworld: pattern
    learn to print a text in a pattern

---
+++ helloworld/pattern-helloworld.cpp
@@ -0,0 +1,20 @@
+/**
+ * learn to print a text in a pattern
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << " H \n";
+    std::cout << " E \n";
+    std::cout << " L \n";
+    std::cout << " L \n";
+    std::cout << " O \n";
+    std::cout << " W \n";
+    std::cout << " O \n";
+    std::cout << " R \n";
+    std::cout << " L \n";
+    std::cout << " D \n";
+
+}
